d8c97020cd82275f6e803763f5968f006048bbb1
Halyna Urbanska <aquilla@ukr.net>
2023-03-29 16:09:53 +0300

    RT-002::Fix DTOs

--- src/dtos/get-text-detection-request.dto.ts
+++ src/dtos/get-text-detection-request.dto.ts
@@ -4,7 +4,7 @@ import { IsOptional } from 'class-validator';
 export class GetTextDetectionRequestDto {
   @ApiProperty({
     type: 'string',
-    default: 'b29925986dcacc97535a5e990a75647bfe4ed52ea45f9d94ee9a7464e3067d35',
+    default: 'JOB_ID',
   })
   joibId: string;
 
